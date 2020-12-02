b21a5f213eed0e6287b63c9b56e2b42e764439f8
Jacin <jacinjiyan@gmail.com>
2020-12-02 23:52:22 +1100

    first commit

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+// initialize plug-ins, preload or eager load
+$(function () {
+    $('#fullpage').fullpage({
+        // set the colour of each section for testing
+        sectionsColor: ['#f00', '#fff', '#ff0', '#fff', '#f0f'],
+        // navigation dots on the right
+        navigation: true,
+    });
+})
