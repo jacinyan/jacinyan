d0c451dc30304da1a970e9c6926b70c1002663f8
Jacin <jacinjiyan@gmail.com>
2020-12-03 02:08:08 +1100

    changed title

--- js/myjs.js
+++ js/myjs.js
@@ -2,8 +2,15 @@
 $(function () {
     $('#fullpage').fullpage({
         // set the colour of each section for testing
-        sectionsColor: ['#f00', '#fff', '#ff0', '#fff', '#f0f'],
+        sectionsColor: ['#060608', '#060608', '#060608', '#060608', '#060608'],
         // navigation dots on the right
         navigation: true,
+        // set a corresponding anchor for each section
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', ],
+        // binding menu. with relevant attributes and the values of anchors set up, a scrolling menu is enabled. Used eventListener
+        menu: '#menu',
+        // loop through the site
+        continuousVertical: true,
+        easingcss3: 'cubic-bezier(0.175, 0.885, 0.032, 1)'
     });
 })
