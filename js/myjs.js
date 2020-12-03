b109725c8ed20406dff12e55b58503fadcdf5ae4
Jacin <jacinjiyan@gmail.com>
2020-12-03 18:07:13 +1100

    started building about page

--- js/myjs.js
+++ js/myjs.js
@@ -11,6 +11,7 @@ $(function () {
         menu: '#menu',
         // loop through the site
         continuousVertical: true,
-        easingcss3: 'cubic-bezier(0.175, 0.885, 0.032, 1)'
+        easingcss3: 'cubic-bezier(0.175, 0.885, 0.032, 1)' ,
+        
     });
 })
