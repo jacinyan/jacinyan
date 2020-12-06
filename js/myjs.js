07d6540acf0ff02508b5611d51c7e7f8ae19645d
Jacin <jacinjiyan@gmail.com>
2020-12-06 16:14:06 +1100

    removed afterload effect

--- js/myjs.js
+++ js/myjs.js
@@ -14,9 +14,41 @@ $(function () {
         // effect curve
         easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1)',
         // Three parameters, new syntax, index starts from 0, anchors are predefined above 
-        afterLoad: function (origin, destination, direction) {
-            var loadedSection = this;
-            //using anchorLink
+        // afterLoad: function (origin, destination, direction) {
+        //     var loadedSection = this;
+        //     //using anchorLink
+        //     if (destination.anchor == 'page1') {
+        //         // $('.greet').addClass('animate__animated animate__fadeIn');
+        //         // $('.who').addClass('animate__animated animate__fadeIn');
+        //     }
+
+        //     if (destination.anchor == 'page2') {
+        //         $('.title').addClass('animate__animated animate__slideInRight');
+        //         $('.headshot').addClass('animate__animated animate__fadeIn');
+        //         $('.text').addClass('animate__animated animate__slideInUp');
+        //     }
+
+        //     if (destination.anchor == 'page3') {
+        //         $('.tech1').addClass('delay1 animate__animated animate__lightSpeedInRight');
+        //         $('.tech2').addClass('delay2 animate__animated animate__lightSpeedInRight');
+        //         $('.tech3').addClass('delay3 animate__animated animate__lightSpeedInRight');
+        //         $('.tech4').addClass('delay4 animate__animated animate__lightSpeedInRight');
+        //         $('.tech5').addClass('delay5 animate__animated animate__lightSpeedInRight');
+        //         $('.tech6').addClass('delay6 animate__animated animate__lightSpeedInRight');
+        //         $('.tech7').addClass('delay7 animate__animated animate__lightSpeedInRight');
+        //         $('.tech8').addClass('delay8 animate__animated animate__lightSpeedInRight');
+        //         $('.tech9').addClass('delay9 animate__animated animate__lightSpeedInRight');
+        //         $('.tech10').addClass('delay10 animate__animated animate__lightSpeedInRight');
+        //     }
+
+        //     if (destination.anchor == 'page4') {
+        //         $('.works').addClass('animate__animated animate__flipInX');
+        //     }
+        // },
+        // onLeave callback is fired once the user leaves a section, in the transition to the new section
+        onLeave: function (origin, destination, direction) {
+            var leavingSection = this;
+
             if (destination.anchor == 'page1') {
                 // $('.greet').addClass('animate__animated animate__fadeIn');
                 // $('.who').addClass('animate__animated animate__fadeIn');
@@ -44,10 +76,6 @@ $(function () {
             if (destination.anchor == 'page4') {
                 $('.works').addClass('animate__animated animate__flipInX');
             }
-        },
-        // onLeave callback is fired once the user leaves a section, in the transition to the new section
-        onLeave: function (origin, destination, direction) {
-            var leavingSection = this;
 
             //after leaving section 2
             if (origin.anchor == 'page1') {
